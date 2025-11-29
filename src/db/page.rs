148f7335f7048ba8932832cf32d570c4d3291a42
diallo <ibrahimasory22@hotmail.com>
2025-11-29 12:20:09 +0100

    debug

--- src/db/page.rs
+++ src/db/page.rs
@@ -498,6 +498,8 @@ impl Page {
             offset_size_header_byte_array as u64,
         );
 
+        println!("VAR_INTS:: {:?}",resp);
+
         let sizes_fields = resp
             .iter()
             .skip(1)
